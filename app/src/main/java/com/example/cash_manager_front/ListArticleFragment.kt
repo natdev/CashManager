5a91cfe9e0b862cccae79b917e946fdcc9dcd420
natdev <dordonne.nathanael@gmail.com>
2019-11-05 09:34:13 -0400

    change fragment paymode

--- app/src/main/java/com/example/cash_manager_front/ListArticleFragment.kt
+++ app/src/main/java/com/example/cash_manager_front/ListArticleFragment.kt
@@ -16,12 +16,12 @@ private const val ARG_PARAM2 = "param2"
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [PayModeFragment.OnFragmentInteractionListener] interface
+ * [ListArticleFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [PayModeFragment.newInstance] factory method to
+ * Use the [ListArticleFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PayModeFragment : Fragment() {
+class ListArticleFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -40,7 +40,7 @@ class PayModeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pay_mode, container, false)
+        return inflater.inflate(R.layout.fragment_list_article, container, false)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -85,12 +85,12 @@ class PayModeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PayModeFragment.
+         * @return A new instance of fragment ListArticleFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PayModeFragment().apply {
+            ListArticleFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
