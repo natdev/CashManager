71ef0eebd1ae77ecaae919dc1a39cf62f5ba32d9
natdev <dordonne.nathanael@gmail.com>
2019-11-09 12:35:46 -0400

    Update menu + fragment

--- app/src/main/java/com/example/cash_manager_front/CartFragment.kt
+++ app/src/main/java/com/example/cash_manager_front/CartFragment.kt
@@ -8,22 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [ListArticleFragment.OnFragmentInteractionListener] interface
+ * [CartFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [ListArticleFragment.newInstance] factory method to
+ * Use the [CartFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListArticleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-
+class CartFragment : Fragment() {
 
 
     override fun onCreateView(
@@ -31,7 +25,7 @@ class ListArticleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_article, container, false)
+        return inflater.inflate(R.layout.fragment_cart, container, false)
     }
-    
+
 }
--- app/src/main/java/com/example/cash_manager_front/PayModeViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cash_manager_front
-
-import androidx.lifecycle.ViewModel
-
-class PayModeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
