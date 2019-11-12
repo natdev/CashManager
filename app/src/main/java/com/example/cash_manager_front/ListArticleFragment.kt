1ef4f93405cfbce29823d5bf4960b8619633b166
nathann zenou <nathannzenou@MacBook-Pro-de-nathann.local>
2019-11-12 13:53:43 +0100

    page d'aceuille, liste objets

--- app/src/main/java/com/example/cash_manager_front/ListArticleFragment.kt
+++ app/src/main/java/com/example/cash_manager_front/ListArticleFragment.kt
@@ -1,12 +1,22 @@
 package com.example.cash_manager_front
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ListAdapter
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_list_article.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,17 +31,42 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ListArticleFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListArticleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class ListArticleFragment : Fragment(),View.OnClickListener {
 
 
+    var articles = arrayOf<String>("gants de boxe", "chaussettes", "iphone")
+    var prices = arrayOf<String>("20", "5", "450")
+    var number = arrayOf<Int>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_article, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
     }
-    
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.fragment_list_article, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // RecyclerView node initialized here
+        articles_recycler_view.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = ArticleAdapter(articles,prices,this)
+        }
+
+    }
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    companion object {
+        fun newInstance(): ListArticleFragment = ListArticleFragment()
+    }
 }
+
